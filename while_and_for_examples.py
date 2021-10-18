f8a14ba6bca3b920b8a64d517fb138be295ae711
dtenenholtz <david.tenenholtz@gmail.com>
2021-10-18 09:43:45 -0700

    initial commit

---
+++ while_and_for_examples.py
@@ -0,0 +1,16 @@
+# basic while and for loops for Python Study Group meeting Oct 2021
+
+# while loop example 1
+number = 0
+while number < 10:
+    print(number)
+    number = number + 2
+
+
+# while loop example 2
+keep_going = 'y'
+while keep_going == 'y':
+    user_says = input('Enter a few words: ')
+    allcaps = user_says.upper().rstrip()
+    print(allcaps)
+    keep_going = input('Do you want to enter another group of words? Type y or n: ')
